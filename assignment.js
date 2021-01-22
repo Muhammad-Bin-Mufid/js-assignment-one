18dafcf6d736543a436446ef973acbc3804455a8
Muhammad Bin Mufid <syyd.mbm@gmail.com>
2021-01-22 16:25:56 +0600

    indent-check

--- assignment.js
+++ assignment.js
@@ -1,26 +1,25 @@
 function kilometerToMeter(inKilometer) {
     if (inKilometer >= 0) {
-        var inMeter = inKilometer * 1000;
-        console.log(inMeter);
+          var inMeter = inKilometer * 1000;
+             console.log(inMeter);
     } else {
         console.log("Invalid input type. Your input was: " + inKilometer + ". Please enter just a pure number without any symbol (ex: -,+,/,* or any other)");
     }
 
-
 }
 
-function budgetCalculator(wristQuantity, mobileQuantity, LaptopQuantity) {
+   function budgetCalculator(wristQuantity, mobileQuantity, LaptopQuantity) {
 
     const wristUnitPrice = 50;
     const mobileUnitPrice = 100;
-    const laptopUnitPrice = 500;
+        const laptopUnitPrice = 500;
 
     let calculatedWristPrice = wristQuantity * wristUnitPrice;
     let calculatedMobilePrice = mobileQuantity * mobileUnitPrice;
     let calculatedLaptopPrice = LaptopQuantity * laptopUnitPrice;
 
     let totalBudget = [calculatedWristPrice, calculatedMobilePrice, calculatedLaptopPrice]
-    console.log(totalBudget);
+        console.log(totalBudget);
 
 }
 
@@ -35,7 +34,7 @@ function hotelCost(daysToStay) {
     let afterSecondTenDaysCost;
 
     if (daysToStay > 0 && daysToStay < 11) {
-        firstTenDaysCost = firstTenDaysBill * daysToStay;
+           firstTenDaysCost = firstTenDaysBill * daysToStay;
         console.log(firstTenDaysCost);
     } else if (daysToStay > 10 && daysToStay < 21) {
         secondTendaysCost = firstTenDaysBill * 10 + secondTenDaysBill * (daysToStay - 10);
@@ -52,11 +51,11 @@ function megaFriend(friendsNameArray) {
     var maxValue = 0;
 
     for (let i = 0; i < friendsNameArray.length; i++) {
-        arraysLengthes[i] = friendsNameArray[i].length;
+            arraysLengthes[i] = friendsNameArray[i].length;
         console.log(arraysLengthes);
         maxValue = Math.max(maxValue, arraysLengthes[i]);
         console.log(maxValue);
-        var some = arraysLengthes.indexOf(maxValue);
+             var some = arraysLengthes.indexOf(maxValue);
         console.log(some);
         console.log(friendsNameArray[some]);
     }
